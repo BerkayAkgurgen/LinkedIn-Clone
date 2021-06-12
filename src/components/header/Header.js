b51f369b61f33a47d2143d431e5dc2a8573964c3
Berkay Akgürgen <akgurgennberkay@gmail.com>
2021-06-12 18:08:07 +0300

    Header oluşturuldu ve stillendirmeleri yapıldı.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,28 +10,29 @@ import HeaderOption from "./HeaderOption";
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__left">
-        <img
-          src="https://image.flaticon.com/icons/png/512/174/174857.png"
-          alt="LinkedIn Logo"
-          height="40"
-        />
-        <div className="header__search">
-          <SearchIcon />
-          <input type="text" />
+      <div className="container flex">
+        <div className="header__left">
+          <img
+            src="https://image.flaticon.com/icons/png/512/174/174857.png"
+            alt="LinkedIn Logo"
+            height="34"
+          />
+          <div className="header__search">
+            <SearchIcon />
+            <input type="text" placeholder="Search" />
+          </div>
         </div>
-      </div>
-      <div className="header__right">
-        <HeaderOption Icon={HomeIcon} title="Home" />
-        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
-        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
-        <HeaderOption Icon={ChatIcon} title="Messaging" />
-        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <div className="header__right">
+          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+          <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+          <HeaderOption Icon={ChatIcon} title="Messaging" />
+          <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+          <HeaderOption avatar="https://media-exp1.licdn.com/dms/image/C4E03AQEW7Q3xbjXniA/profile-displayphoto-shrink_400_400/0/1619898269205?e=1628726400&v=beta&t=NK_g6mYtsq_T7D9fmGDuzFMqjKTlppZF8BYjrPCuTr0"  title="Me"/>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Header;
-
-// 56.33
--- src/components/header/HeaderOption.js
+++ src/components/header/HeaderOption.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Avatar } from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
-const HeaderOption = ({ title, Icon }) => {
+const HeaderOption = ({ avatar, title, Icon }) => {
   return (
     <div className="header__option">
       {Icon && <Icon className="header__option-icon" />}
-      <h3 className="header__option-title">{title}</h3>
+      {avatar && <Avatar className="header__option-icon" src={avatar} />}
+      {title === "Me" ? (
+        <div className="header__option-dropdown">
+          <h3 className="header__option-title">{title}</h3>
+          <ArrowDropDownIcon />
+        </div>
+      ) : (
+        <h3 className="header__option-title">{title}</h3>
+      )}
     </div>
   );
 };
