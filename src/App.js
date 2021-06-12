154aa38670e493cc2196f8955222d3449ccc18d8
Berkay Akgürgen <akgurgennberkay@gmail.com>
2021-06-12 20:55:15 +0300

    Left sidebar componenti eklendi ve stil düzenlemesi yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/header/Header";
+import SideBar from "./components/sidebar/SideBar";
 import "./styles/App.scss";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="app__body">
+        <SideBar />
+      </div>
     </div>
   );
 }
