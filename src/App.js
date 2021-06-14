0eb73d03a01d992839811d57f69ed539bbb7eb7a
Berkay Akgürgen <akgurgennberkay@gmail.com>
2021-06-14 23:43:55 +0300

    Redux ve firebase auth entegre edildi.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Header from "./components/header/Header";
 import SideBar from "./components/sidebar/SideBar";
 import Feed from "./components/feed/form/Feed";
+import { selectUser } from "./features/userSlice";
 import "./styles/App.scss";
+import Login from "./components/login/Login";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <SideBar />
-        <Feed />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <SideBar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
